55886eaa772a7e1dd994bfea369a945bf891d7e4
凉宫枫 <149847088+Arisgod1@users.noreply.github.com>
2025-06-01 15:15:03 +0800

    Initial commit

---
+++ src/main/java/com/tmd/ai/config/CommonConfiguration.java
@@ -0,0 +1,18 @@
+package com.tmd.ai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class CommonConfiguration {
+    @Bean
+    public ChatClient chatClient(OllamaChatModel model){
+        return ChatClient
+                .builder(model)
+                .build();
+    }
+
+}
---
+++ src/test/java/com/tmd/ai/AgentAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmd.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
