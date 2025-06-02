7c3e805867df5bc2603c6dedf527249308810c72
凉宫枫 <149847088+Arisgod1@users.noreply.github.com>
2025-06-02 17:06:09 +0800

    完成了会话历史记录功能，可以细分成先完成了对会话ID的保存，然后完成了通过会话ID对数据进行查询的功能。

--- src/main/java/com/tmd/ai/controller/ChatController.java
+++ src/main/java/com/tmd/ai/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.tmd.ai.controller;
 
+import com.tmd.ai.repository.ChatHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,14 @@ import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvis
 public class ChatController {
 
     private final ChatClient chatClient;
+
+    private final ChatHistoryRepository chatHistoryRepository;
+
     @RequestMapping(value = "/chat", produces = "text/html;charset=utf-8")
     public Flux<String> chat(String prompt, String chatId){
+        //1.保存会话ID
+        chatHistoryRepository.save("chat",chatId);
+        //2.请求模型
         return chatClient.prompt()
                 .user(prompt)
                 .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY,chatId))
