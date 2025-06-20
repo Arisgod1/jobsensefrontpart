3754dadd191025988353c818e6810a4632d28b08
凉宫枫 <149847088+Arisgod1@users.noreply.github.com>
2025-06-20 17:23:05 +0800

    完成了会话历史记录功能，可以细分成先完成了对会话ID的保存，然后完成了通过会话ID对数据进行查询的功能。

--- src/main/java/com/tmd/ai/config/CommonConfiguration.java
+++ src/main/java/com/tmd/ai/config/CommonConfiguration.java
@@ -1,11 +1,13 @@
 package com.tmd.ai.config;
 
+import com.tmd.ai.constants.SystemConstants;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +24,19 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫张鸿昊,请以张鸿昊的身份和语气回答问题")
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        new MessageChatMemoryAdvisor(chatMemory)
+
+                )
+                .build();
+    }
+
+    @Bean
+    public ChatClient gamechatClient(OpenAiChatModel model, ChatMemory chatMemory){
+        return ChatClient
+                .builder(model)
+                .defaultSystem(SystemConstants.GAME_SYSTEM_PROMPT)
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
                         new MessageChatMemoryAdvisor(chatMemory)
