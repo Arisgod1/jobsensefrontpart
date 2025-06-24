7748dfc9cf30fb69c19baa33b6917e0203a13a56
Arisgod1 <2934487705@qq.com>
2025-06-24 21:48:30 +0800

    增加了接收pdf文件保存在本地作为挂载知识库进行问答的功能

--- src/main/java/com/tmd/ai/AgentAiApplication.java
+++ src/main/java/com/tmd/ai/AgentAiApplication.java
@@ -1,10 +1,11 @@
 package com.tmd.ai;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
+@MapperScan("com.tmd.ai.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AgentAiApplication {
 
--- src/main/java/com/tmd/ai/config/MvcConfiguration.java
+++ src/main/java/com/tmd/ai/config/MvcConfiguration.java
@@ -12,6 +12,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .exposedHeaders("Content-Disposition");;
     }
 }
