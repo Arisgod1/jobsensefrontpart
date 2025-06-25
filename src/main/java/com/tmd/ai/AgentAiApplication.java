dedb58a52e7a97c0eae3480b5cfe8658a131161b
Arisgod1 <2934487705@qq.com>
2025-06-25 10:21:24 +0800

    增加了接收pdf文件保存在本地作为挂载知识库进行问答的功能

--- src/main/java/com/tmd/ai/AgentAiApplication.java
+++ src/main/java/com/tmd/ai/AgentAiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@MapperScan("com.tmd.ai.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AgentAiApplication {
 
